6a695d94b2267d665925e3c9a73f8d9a8b5e9f9c
Junyu Chen <jchen245@jhmi.edu>
2022-01-14 02:59:42 -0500

    Fixed bugs

--- FCM_loss/torch/FCM_losses.py
+++ FCM_loss/torch/FCM_losses.py
@@ -7,33 +7,31 @@ class FCM_label(nn.Module):
     def __init__(self, fuzzy_factor=2, one_hot=True):
         '''
         Supervised Fuzzy C-mean loss function for ConvNet based image segmentation
-
         Junyu Chen, et al. Learning Fuzzy Clustering for SPECT/CT Segmentation
         via Convolutional Neural Networks. Medical physics, 2021 (In press).
-
         :param fuzzy_factor: exponent for controlling fuzzy overlap, default value = 2
         :param one_hot: setting to True will convert truth into one hot encoding
-
         :param y_pred: prediction from ConvNet, assuming that SoftMax has been applied.
-        :param y_true: ground thruth label
+        :param y_true: ground truth label
         '''
         super().__init__()
         self.fuzzy_factor = fuzzy_factor
         self.one_hot = one_hot
 
     def forward(self, y_pred, y_true):
+        B, C, W, H, L = y_pred.shape
         dim = len(list(y_pred.shape)[2:])
         assert dim == 3 or dim == 2, 'Supports only 3D or 2D!'
-        num_clus = list(y_pred.shape)[1]
+        num_clus = C
         if self.one_hot:
-            y_true = F.one_hot(y_true, num_classes=y_pred.shape[1])
+            y_true = F.one_hot(y_true.long(), num_classes=num_clus)
             y_true = torch.squeeze(y_true, 1)
             if dim == 3:
                 y_true = y_true.permute(0, 4, 1, 2, 3)
             else:
                 y_true = y_true.permute(0, 3, 1, 2)
-        pred = torch.reshape(y_pred, [-1, num_clus, math.prod(list(y_true.shape)[2:])])
-        truth = torch.reshape(y_true, (-1, num_clus, math.prod(list(y_true.shape)[2:])))
+        pred = torch.reshape(y_pred, (B, num_clus, math.prod(list(y_true.shape)[2:])))
+        truth = torch.reshape(y_true, (B, num_clus, math.prod(list(y_true.shape)[2:])))
         J_1 = 0
         for i in range(num_clus):
             label = truth[:, i, :]
@@ -41,18 +39,14 @@ class FCM_label(nn.Module):
             J_1 += mem*torch.pow(label - 1, 2)
         return torch.mean(J_1/num_clus)
 
-
 class RFCM_loss(nn.Module):
     def __init__(self, fuzzy_factor=2, regularizer_wt=0.0008):
         '''
         Unsupervised Robust Fuzzy C-mean loss function for ConvNet based image segmentation
-
         Junyu Chen, et al. Learning Fuzzy Clustering for SPECT/CT Segmentation
         via Convolutional Neural Networks. Medical physics, 2021 (In press).
-
         :param fuzzy_factor: exponent for controlling fuzzy overlap, default value = 2
         :param regularizer_wt: weighting parameter for regularization, default value = 0
-
         Note that ground truth segmentation is NOT needed in this loss fuction, instead, the input image is required.
         :param y_pred: prediction from ConvNet, assuming that SoftMax has been applied.
         :param image: input image to the ConvNet.
@@ -65,9 +59,9 @@ class RFCM_loss(nn.Module):
     def forward(self, y_pred, image):
         dim = len(list(y_pred.shape)[2:])
         assert dim == 3 or dim == 2, 'Supports only 3D or 2D!'
-        num_clus = list(y_pred.shape)[1]
-        pred = torch.reshape(y_pred, (-1, num_clus, math.prod(list(y_pred.shape)[2:]))) #(bs, C, V)
-        img = torch.reshape(image, (-1, math.prod(list(image.shape)[2:]))) #(bs, V)
+        num_clus = y_pred.shape[1]
+        pred = torch.reshape(y_pred, (y_pred.shape[0], num_clus, math.prod(list(y_pred.shape)[2:]))) #(bs, C, V)
+        img = torch.reshape(image, (y_pred.shape[0], math.prod(list(image.shape)[2:]))) #(bs, V)
         if dim == 3:
             kernel = torch.ones((1, 1, 3, 3, 3)).float().cuda()
             kernel[:, :, 1, 1, 1] = 0
